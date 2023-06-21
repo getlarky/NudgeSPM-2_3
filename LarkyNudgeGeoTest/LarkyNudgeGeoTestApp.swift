e0d33599e34911676f4d301b1c3a71b33669ec3e
juan9519 <juanrod9519@icloud.com>
2023-06-21 20:27:06 -0300

    Initial Commit

---
+++ LarkyNudgeGeoTest/LarkyNudgeGeoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LarkyNudgeGeoTestApp.swift
+//  LarkyNudgeGeoTest
+//
+//  Created by Juan Rodriguez on 21/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LarkyNudgeGeoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
